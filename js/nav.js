cf7292c1ada88bb88b99bb1b4ec2fc2d997d52d1
Jefferson Campos <cjminer544@gmail.com>
2019-11-04 19:32:37 -0400

    home completo y primero de nosotros

--- js/nav.js
+++ js/nav.js
@@ -8,14 +8,3 @@ btnmenu.addEventListener('click', () => {
     menu.classList.toggle('getMenu');
 });
 
-
-window.onscroll = () => {
-    // Obtenemos la posicion del scroll en pantalla
-    var scroll = document.documentElement.scrollTop || document.body.scrollTop;
-
-    // Realizamos alguna accion cuando el scroll este en 100px
-    if(scroll > 200){
-        btnUp.classList.values('getUp');
-        console.log('entro')
-    }
-}
