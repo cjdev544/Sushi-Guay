8ce1d0646346aa07b371d52ccf4fac1ada9f575d
Jefferson Campos <cjminer544@gmail.com>
2019-11-04 10:03:40 -0400

    Pagina home con logo y color original

--- js/nav.js
+++ js/nav.js
@@ -2,7 +2,20 @@
 
 let btnmenu = document.getElementById('btnmenu');
 let menu = document.getElementById('menu');
+let btnUp = document.getElementById('btnUp');
 
 btnmenu.addEventListener('click', () => {
     menu.classList.toggle('getMenu');
 });
+
+
+window.onscroll = () => {
+    // Obtenemos la posicion del scroll en pantalla
+    var scroll = document.documentElement.scrollTop || document.body.scrollTop;
+
+    // Realizamos alguna accion cuando el scroll este en 100px
+    if(scroll > 200){
+        btnUp.classList.values('getUp');
+        console.log('entro')
+    }
+}
